5d9ff9bd197f606a0114e0c90642687b154e0e42
makelick <vi.neshcheret@gmail.com>
2022-02-20 00:25:25 +0200

    init structs

--- lab_cpp_2/Functions.cpp
+++ lab_cpp_2/Functions.cpp
@@ -1,2 +1,18 @@
 #include "Header.h"
 
+struct date {
+	int day;
+	int month;
+	int year;
+};
+
+struct employee {
+	string surname;
+	date birthday;
+	date start_career;
+};
+
+void input_file(string file_path)
+{
+
+}
--- lab_cpp_2/Header.h
+++ lab_cpp_2/Header.h
@@ -1,4 +1,12 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <string>
 
+using namespace std;
+
+struct employee;
+struct date;
+
+void input_file(string);
+void output_file(string);
