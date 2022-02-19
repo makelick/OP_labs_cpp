e62c133b9f7491ce902182f2c4ed48d12bbdfaab
makelick <vi.neshcheret@gmail.com>
2022-02-20 00:25:25 +0200

    init structs

--- lab_cpp_2/lab_cpp_2.cpp
+++ lab_cpp_2/lab_cpp_2.cpp
@@ -2,7 +2,12 @@
 
 int main()
 {
-    
+    string first_file_path = "first file.txt";
+    string second_file_path = "second file.txt";
+    input_file(first_file_path);
+
+    cout << "First file:";
+    output_file(first_file_path);
 
     return 0;
 }
