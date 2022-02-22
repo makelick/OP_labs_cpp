1657ce34ad895e3a382004f297fcde28fc98ffd4
makelick <vi.neshcheret@gmail.com>
2022-02-22 03:08:15 +0200

    refactor and complete create_second_file

--- lab_cpp_2/lab_cpp_2.cpp
+++ lab_cpp_2/lab_cpp_2.cpp
@@ -6,8 +6,14 @@ int main()
     string second_file_path = "second file.txt";
     input_file(first_file_path);
 
-    cout << "Birthday in this month and career >= 5 years: " << endl;
+    cout << "Birthday in this month AND work experience >= 5 years: " << endl;
     birthday_in_this_month(first_file_path);
+    cout << endl;
+
+    create_second_file(first_file_path, second_file_path);
+
+    cout << "Start career in <= 25 yo AND work experience >= 10 years (second file):" << endl;
+    output_file(second_file_path);
 
     return 0;
 }
