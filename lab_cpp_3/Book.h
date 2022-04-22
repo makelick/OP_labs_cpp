037c27224b5fad32320d51f10566430ffbebd436
makelick <vi.neshcheret@gmail.com>
2022-04-22 19:18:13 +0300

    add files for lab 3

---
+++ lab_cpp_3/Book.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Book {
+private:
+	string author;
+	string name;
+	int year;
+	int numberOfPages;
+
+private:
+
+};
---
+++ lab_cpp_3/lab_cpp_3.cpp
@@ -0,0 +1,7 @@
+#include "Book.h"
+
+int main()
+{
+    
+    return 0;
+}
