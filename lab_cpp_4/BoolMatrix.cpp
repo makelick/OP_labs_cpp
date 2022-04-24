ba615e09900d4671121178ce2da8a0842fc4ade1
makelick <vi.neshcheret@gmail.com>
2022-04-24 21:02:52 +0300

    add constructors for BoolMatrix

--- lab_cpp_4/BoolMatrix.cpp
+++ lab_cpp_4/BoolMatrix.cpp
@@ -1,2 +1,62 @@
 #include "BoolMatrix.h"
 
+int BoolMatrix::getRows() { return rows; };
+
+int BoolMatrix::getCols() { return cols; };
+
+bool** BoolMatrix::getMatrix() { return matrix; };
+
+BoolMatrix::BoolMatrix(int rws, int cls) {
+    rows = rws;
+    cols = cls;
+    matrix = new bool* [rows];
+    for (int i = 0; i < rows; i++)
+    {
+        matrix[i] = new bool[cols];
+    }
+
+    srand(time(NULL));
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i][j] = rand() % 2;
+        }
+    }
+};
+
+BoolMatrix::BoolMatrix(int rws, int cls, bool** matr) {
+    rows = rws;
+    cols = cls;
+    matrix = new bool* [rows];
+    for (int i = 0; i < rows; i++)
+    {
+        matrix[i] = new bool[cols];
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i][j] = matr[i][j];
+        }
+    }
+};
+
+BoolMatrix::BoolMatrix(BoolMatrix& other) {
+    rows = other.rows;
+    cols = other.cols;
+    matrix = new bool* [rows];
+    for (int i = 0; i < rows; i++)
+    {
+        matrix[i] = new bool[cols];
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i][j] = other.matrix[i][j];
+        }
+    }
+};
+
+BoolMatrix::~BoolMatrix() {
+    for (int i = 0; i < rows; i++)
+    {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+};
--- lab_cpp_4/BoolMatrix.h
+++ lab_cpp_4/BoolMatrix.h
@@ -1,5 +1,21 @@
 #pragma once
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+class BoolMatrix {
+private:
+	int rows;
+	int cols;
+	bool** matrix;
+public:
+	BoolMatrix(int, int);
+	BoolMatrix(int, int, bool**);
+	BoolMatrix(BoolMatrix&);
+	~BoolMatrix();
+	int getRows();
+	int getCols();
+	bool** getMatrix();
+};
