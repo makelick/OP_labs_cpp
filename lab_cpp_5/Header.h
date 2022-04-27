dde612327ac1577e682c8b6130bcbe3c81807825
makelick <vi.neshcheret@gmail.com>
2022-04-27 20:02:31 +0300

    describe classes and their methods

--- lab_cpp_5/Header.h
+++ lab_cpp_5/Header.h
@@ -1,5 +1,52 @@
 #pragma once
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+class BankAccount {
+private:
+	string bankName;
+	static int idGenerator;
+	int id;
+	bool isAvailable;
+public:
+	virtual void addBalance(int) = 0;
+	virtual void subBalance(int) = 0;
+};
+
+class CurrentAccount : public BankAccount {
+private:
+	Date lastOperationDate;
+	int balance;
+public:
+	virtual void addBalance(int sum) { balance += sum; };
+	virtual void subBalance(int sum) { balance -= sum; };
+};
+
+class DepositAccount : public BankAccount {
+private:
+	Date openingDate;
+	int period;
+	int balance;
+	double rate;
+public:
+	virtual void addBalance(int sum) { balance += sum; };
+	virtual void subBalance(int sum) { balance -= sum; };
+};
+
+class Date {
+private:
+	int year;
+	int month;
+	int day;
+public:
+	int getYear() { return year; }
+	int getMonth() { return month; }
+	int getDay() { return day; }
+	void setYear(int year) { this->year = year; }
+	void setMonth(int month) { this->month = month; }
+	void setDay(int day) { this->day = day; }
+	friend int getMonthsBetweenDates(Date&, Date&);
+};
--- lab_cpp_5/Source.cpp
+++ lab_cpp_5/Source.cpp
@@ -1,2 +1,5 @@
 #include "Header.h"
 
+int getMonthsBetweenDates(Date& date1, Date& date2) {
+	return ((date1.year - date2.year) * 12) + date1.month - date2.month;
+}
